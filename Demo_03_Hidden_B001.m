cb69c3594d8d448ebba9ac7b22f17b89a644432f
Feng-Kuei Chiang <fkchiang@gmail.com>
2022-04-20 10:39:07 -0400

    Create Demo_03_Hidden_B001.m

---
+++ Demo_03_Hidden_B001.m
@@ -0,0 +1,10 @@
+clear;clc;close all;
+% 1. create an example matrix with NaNs
+eMtx = randi(5,3,5); % [trials, saccades]
+eMtx(3,[2,4]) = nan; 
+% 2. would like to change this matrix to a column vector and sort it by trials first and then saccades.
+%    the length of this column vector is equal to the numbers of values with no NaNs.
+temp = eMtx';
+cVec = temp(:);
+idx_nan = isnan(cVec);
+cVec = cVec(~idx_nan);
