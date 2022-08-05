ddee1b4641d8e8b958a2cd61db8d8d9808b570ee
Feng-Kuei Chiang <fkchiang@gmail.com>
2022-08-05 16:58:15 -0400

    update

--- Demo_00_Hidden_Bdata.m
+++ Demo_00_Hidden_Bdata.m
@@ -1,13 +1,5 @@
-%% created for preprocess the bhv2 files.
-% modified, FKC, 09/07/2019
-% changed path, FKC, 7/11/2021
-% saved, FKC, 7/11/2021 for SfN 2021
-% saved, FKC, Github
-% saved, by vscode and matlab/again
-% checked func_mlread_blockwise, FKC, 05/25/2022
-% checked and saved by FKC, 6/25/2022 for tgEight_blkwFKC in A & B
+%% created by FKC, 8/05/2022
 clear;clc;close all;
-% cd /home/fengkuei/04_tgEight_blkw % 6/25/2022 @ the brain
 cd G:\
 for Subject = 1
     if Subject == 1
--- config_plot_sample.m
+++ config_plot_sample.m
@@ -1,8 +1,8 @@
 % sample plot
 clear;clc;close all;
-load('Hidden_B001','Hidden_B001','tgEyeData');
+load('Hidden_oldB001.mat','Hidden_B001','tgEyeData');
 load('dotsEIGHTfinal.mat', 'config80');
 config = config80;
 Trial = 2;
 func_ConfigPlot_w_Eye(Trial,Hidden_B001,tgEyeData,config);
-print(gcf,'-dpdf','-r300','-bestfit','Hidden_B001_block#01_sample_trial#02');
+% print(gcf,'-dpdf','-r300','-bestfit','Hidden_B001_block#01_sample_trial#02');
