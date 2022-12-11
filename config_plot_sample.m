721eab68aba8c361b6ecea3562ce42dfc18c4db0
Feng-Kuei Chiang <fkchiang@gmail.com>
2022-12-11 11:03:56 -0500

    update

--- config_plot_sample.m
+++ config_plot_sample.m
@@ -3,6 +3,6 @@ clear;clc;close all;
 load('Hidden_B001','Hidden_B001','tgEyeData');
 load('dotsEIGHTfinal.mat', 'config80');
 config = config80;
-Trial = 2;
+Trial = 12;
 func_ConfigPlot_w_Eye(Trial,Hidden_B001,tgEyeData,config);
-print(gcf,'-dpdf','-r300','-bestfit','Hidden_B001_block#01_sample_trial#02');
+% print(gcf,'-dpdf','-r300','-bestfit','Hidden_B001_block#01_sample_trial#02');
